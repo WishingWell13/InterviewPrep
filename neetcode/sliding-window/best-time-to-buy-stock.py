2cccc9b5188b61de3004a73d897b3cd4878d08e6
Andrew Smithwick <andy.smithwick@gmail.com>
2025-08-13 22:05:00 -0700

    Create best-time-to-buy-stock.py

---
+++ neetcode/sliding-window/best-time-to-buy-stock.py
@@ -0,0 +1,22 @@
+'''
+Input: prices = [10,1,5,6,7,1]
+                b   b       e
+Output: 6
+'''
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buyDay = 0
+        sellDay = 0 #inclusive
+        bestProfit = 0
+
+
+        while sellDay < len(prices):
+            if prices[sellDay] < prices[buyDay]:
+                buyDay = sellDay
+
+            bestProfit = max(prices[sellDay] - prices[buyDay], bestProfit)
+            sellDay += 1
+
+        return bestProfit
